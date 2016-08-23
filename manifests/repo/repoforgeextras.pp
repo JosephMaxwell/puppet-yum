a7e2015fc88d6b073a2ab678de6bf71844a5f172
Joseph Maxwell <joseph@swiftotter.com>
2016-08-23 15:55:11 -0500

    Replacing references to `apt.sw.be`

--- manifests/repo/repoforgeextras.pp
+++ manifests/repo/repoforgeextras.pp
@@ -1,14 +1,14 @@
 # = Class: yum::repo::repoforgeextras
 #
 # This class installs the repoforge extras repo
-#
+# 
 class yum::repo::repoforgeextras {
 
   $osver = split($::operatingsystemrelease, '[.]')
 
   yum::managed_yumrepo { 'repoforgeextras':
     descr    => 'RepoForge extra packages',
-    baseurl  => "http://apt.sw.be/redhat/el${osver[0]}/en/\$basearch/extras",
+    baseurl  => "http://repository.it4i.cz/mirrors/repoforge/redhat/el${osver[0]}/en/\$basearch/extras",
     enabled  => 1,
     gpgcheck => 1,
     gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
